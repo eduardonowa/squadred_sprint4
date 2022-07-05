4a67d6851c394bde53250ee8d13eea3af0e4bc04
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-07-05 15:01:30 -0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  // eslint-disable-next-line
+  name: "Home",
+  components: {},
+};
+</script>
