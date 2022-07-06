80af2b6c240a8bd50e4feb0803c701f9556ed14f
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-07-05 21:08:45 -0300

    feat: creating button component

---
+++ src/components/micro/Button/Button.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="button-container">
+    <button
+      class="classButton"
+      v-if="type === '0'"
+      id="button0"
+      @click.prevent="event"
+    >
+      {{ msg }} <img class="img0" src="@/assets/next.png" alt="next image" />
+    </button>
+    <button
+      class="classButton"
+      v-else-if="type === '1'"
+      id="button1"
+      @click.prevent="event"
+    >
+      <img src="@/assets/check.png" class="img1" alt="check image" />{{ msg }}
+    </button>
+    <button
+      class="classButton"
+      v-else-if="type === '2'"
+      id="button2"
+      @click.prevent="event"
+    >
+      <img src="@/assets/plus.png" class="img2" alt="plus image" />{{ msg }}
+      <img src="@/assets/next.png" class="img0" alt="next image" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "Button",
+  props: {
+    msg: String,
+    type: String,
+    event: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scooped>
+@import "./Button.scss";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+
 export default {
   // eslint-disable-next-line
-  name: "Home",
-  components: {},
-};
-</script>
+  name: 'Home',
+  components: {
+  }
+}
