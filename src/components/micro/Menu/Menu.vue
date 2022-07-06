f0d57c0cdd48e39a0ec4ef5661797da5f59ecc38
PedroLDallIgna <pedro.lucas-di@hotmail.com>
2022-07-06 09:36:00 -0300

    refactor: new logic implemented at tab change

--- src/components/micro/Menu/Menu.vue
+++ src/components/micro/Menu/Menu.vue
@@ -1,36 +1,103 @@
 <template>
   <nav class="menu">
-    <div class="menu-tab">
-        <button class="basic" @click="loadBasicTab" :disabled="!abledTabs.includes('Basic')">Basic</button>
+    <div 
+      class="menu-tab" 
+      v-for="(tab, i) in $store.state.menuTabs"
+      :key="i"
+      >
+        <button 
+          v-if="i === 0" 
+          :class="tab + ' focus'" 
+          @click="loadTab(tab)" 
+          :disabled="!abledTabs.includes(tab)"
+          >
+            {{ capitalize(tab) }}
+          </button>
+        <button 
+          v-else 
+          :class="tab" 
+          @click="loadTab(tab)" 
+          :disabled="!abledTabs.includes(tab)"
+          >
+            {{ capitalize(tab) }}
+        </button>
+    </div>
+    <button @click="next">Next</button>
+    <!-- <div class="menu-tab">
+        <button class="basic focus" @click="loadBasicTab" :disabled="!abledTabs.includes('Basic')">Basic</button>
     </div>
     <div class="menu-tab">
         <button class="social" @click="loadSocialTab" :disabled="!abledTabs.includes('Social')">Social</button>
     </div>
     <div class="menu-tab">
         <button class="certificates" @click="loadCertificatesTab" :disabled="!abledTabs.includes('Certificates')">Certificates</button>
-    </div>
+    </div> -->
   </nav>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
     name: "MenuComponent",
-    props: {
-        abledTabs: {
-            type: Array,
-            default: () => ['Basic']
+    data() {
+        return {
+            abledTabs: this.$store.state.abledTabs,
+            actualTab: null
         }
     },
+    // props: {
+    //     abledTabs: {
+    //         type: Array,
+    //         default: () => ['basic']
+    //     }
+    // },
+    mounted() {
+        this.loadTab(this.abledTabs[0]);
+    },
     methods: {
-        loadBasicTab() {
-            console.log('Basic');
+        ...mapActions(['ableTab']),
+        // loadBasicTab() {
+        //     console.log('Basic');
+        //     this.focused('basic');
+        // },
+        // loadSocialTab() {
+        //     console.log('Social');
+        //     this.focused('social');
+        // },
+        // loadCertificatesTab() {
+        //     console.log('Certificates');
+        //     this.focused('certificates');
+        // },
+        focused(className) {
+            this.abledTabs.forEach((tab) => {
+                const el = document.querySelector(`.${tab.toLowerCase()}`);
+                el.classList.remove('focus');
+            });
+            const el = document.querySelector(`.${className}`);
+            el.classList.add('focus');
         },
-        loadSocialTab() {
-            console.log('Social');
+        loadTab(tab) {
+            console.log(tab);
+            this.focused(tab);
+            this.actualTab = tab;
+            // console.log(this.$store.state.menuTabs.indexOf(tab));
+            // this.ableTab(this.$store.state.menuTabs[this.$store.state.menuTabs.indexOf(tab) + 1]);
+            console.log(this.$store.state.abledTabs);
+            this.abledTabs = this.$store.state.abledTabs;
         },
-        loadCertificatesTab() {
-            console.log('Certificates');
+        capitalize(word) {
+            let letters = word.split('');
+            let first = letters.shift();
+            letters.unshift(first.toUpperCase());
+            return letters.join('');
         },
+        next() {
+            if (!(this.$store.state.menuTabs.indexOf(this.actualTab) + 1 === this.$store.state.menuTabs.length)) {
+                this.ableTab(this.$store.state.menuTabs[this.$store.state.menuTabs.indexOf(this.actualTab) + 1])
+            } else {
+                console.log('última');
+            }
+        }
     }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    menuTabs: ['basic', 'social', 'certificates'],
+    abledTabs: ['basic']
   },
   getters: {
   },
   mutations: {
+    ableTab(state, tab) {
+      state.abledTabs.push(tab);
+    }
   },
   actions: {
+    ableTab({commit}, tab) {
+      commit('ableTab', tab);
+    }
   },
   modules: {
   }
