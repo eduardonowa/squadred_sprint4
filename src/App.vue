0881564ff9359563930cb91dc59bed8cf189d596
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-07-08 20:16:31 -0300

    feat: removing background on chrome autocomplete

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,18 @@
   padding: 0;
   box-sizing: border-box;
 }
+
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+textarea:-webkit-autofill,
+textarea:-webkit-autofill:hover,
+textarea:-webkit-autofill:focus,
+select:-webkit-autofill,
+select:-webkit-autofill:hover,
+select:-webkit-autofill:focus {
+  -webkit-text-fill-color: #767676;
+  -webkit-box-shadow: none;
+  transition: background-color 5000s ease-in-out 0s;
+}
 </style>
