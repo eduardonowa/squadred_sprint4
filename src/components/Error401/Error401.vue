37800a7e36b4d25a9e003fd7225699176d153fbd
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-07-10 18:59:22 -0300

    feat: adding error 404 and 401 pages

---
+++ src/components/Error401/Error401.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="erro disable-select">
+    <img
+      src="@/assets/401 Error Unauthorized.gif"
+      alt="error image"
+      class="errorimg"
+    />
+    <h3>Você será redirecionado em {{ time }} s...</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "Error401",
+  data() {
+    return {
+      time: 7,
+    };
+  },
+  methods: {
+    redirect() {
+      this.time--;
+      if (this.time == 0) {
+        this.$router.push("/");
+      }
+    },
+  },
+  mounted() {
+    this.$store.state.loggedUser = false;
+    window.localStorage.getItem("loggedUser", this.$store.state.loggedUser);
+    window.localStorage.setItem("username", "");
+    window.localStorage.setItem("password", "");
+
+    setInterval(() => {
+      this.redirect();
+    }, 1000);
+  },
+};
+</script>
+
+<style lang="scss" scooped>
+@import "./Error401.scss";
+</style>
---
+++ src/components/Error404/Error404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="erro404">
+    <img src="@/assets/error404.gif" alt="Erro 404" class="errorgif" />
+    <h2>Página não encontrada</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "Error404",
+};
+</script>
+
+<style lang="scss" scooped>
+@import "./Error404.scss";
+</style>
