3622a802301cd90252a081f166a242dbd43857ca
Joao Treto <joaoatreto@gmail.com>
2022-07-12 13:13:28 -0300

    feat: adding local storage in certificates tab

--- src/components/containers/Tabs/Certificates/Certificates.vue
+++ src/components/containers/Tabs/Certificates/Certificates.vue
@@ -4,6 +4,8 @@
       LabelInput="Certificates"
       ClassField="certificates"
       Placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/"
+      Type="text"
+      :valueInput="certificatesValue"
     />
     <div class="buttons">
       <Button msg="Certificate" type="3" class="certificateButton"></Button>
@@ -16,6 +18,7 @@
       InvalidText="Please enter your Team Name"
       Placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/"
       Type="text"
+      :valueInput="teamnameValue"
     />
     <MyInputs
       LabelInput="Institution *"
@@ -24,6 +27,7 @@
       InvalidText="Please enter your Institution"
       Placeholder="Universidade Federal da Paraíba"
       Type="text"
+      :valueInput="institutionValue"
     />
     <MyInputs
       LabelInput="Graduation *"
@@ -32,24 +36,50 @@
       InvalidText="Please enter your Graduation"
       Placeholder="Ciências da Computação"
       Type="text"
+      :valueInput="graduationValue"
     />
+    <Button class="finish" msg="Finish" type="1"></Button>
   </div>
 </template>
 
 <script>
 import MyInputs from "@/components/MyInputs/MyInputs.vue";
 import Button from "@/components/micro/Button/Button.vue";
+import { mapActions } from "vuex";
+
 export default {
   // eslint-disable-next-line
   name: "Certificates",
-  components: {
-    MyInputs,
-    Button,
+  components: { MyInputs, Button, },
+  data(){
+    return{
+      certificatesValue: "",
+      teamnameValue: "",
+      institutionValue: "",
+      graduationValue: "",
+    };
   },
   mounted(){
-    document.title = `${process.env.VUE_APP_TITLE} | Certificates`
+    document.title = `${process.env.VUE_APP_TITLE} | Certificates`;
 
-  }
+    this.certificatesValue = window.localStorage["certificates"];
+    this.teamnameValue = window.localStorage["teamname"];
+    this.institutionValue = window.localStorage["institution"];
+    this.graduationValue = window.localStorage["graduation"];
+  },
+  methods: {
+    ...mapActions(["nextTab"]),
+    validate() {
+      if(
+        window.localStorage["certificates"] &&
+        window.localStorage["teamname"] &&
+        window.localStorage["institution"] &&
+        window.localStorage["graduation"]
+      ) {
+        this.nextTab();
+      }
+    }
+  },
 };
 </script>
 
